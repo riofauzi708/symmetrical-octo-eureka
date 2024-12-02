8035bda9537a1ca5e5b733903440fc8ef2e9d189
Rio <riofauzi708@gmail.com>
2024-12-02 11:47:19 +0700

    initial commit

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import API_KEY from '../../config';
 
 const MovieDetail = () => {
   const router = useRouter();
@@ -8,7 +9,7 @@ const MovieDetail = () => {
 
   useEffect(() => {
     if (id) {
-      fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=84053d7ee344c3731b571bbd807b1ee4`)
+      fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`)
         .then(response => response.json())
         .then(data => setMovie(data))
         .catch(error => console.error('Error:', error));
@@ -20,14 +21,13 @@ const MovieDetail = () => {
       {movie ? (
         <div className="movie-detail">
           <h1>{movie.title}</h1>
-          <iframe
-            width="100%"
-            height="500"
-            src="URL_YOUTUBE_VIDEO"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+          <video controls width="100%">
+            {/* Kode iklan video */}
+            <source src="https://example.com/path/to/ad.mp4" type="video/mp4" />
+            {/* Kode video film */}
+            <source src="https://example.com/path/to/film.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
           <p>{movie.overview}</p>
           <p>Release Date: {movie.release_date}</p>
           <p>Rating: {movie.vote_average}</p>
